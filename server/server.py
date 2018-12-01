2e7713679b4fd9577245f970fcac741db9a0184b
Morgan-iv <ivushkinalex@gmail.com>
2018-12-01 19:28:49 +0300

    Server v0.1.0

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import socketserver
-import ml
+#import ml
 from enum import Enum
 
 messages = {}
@@ -29,7 +29,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def Process_USER_INIT(self):
         global messages
-        pubksize = 2048
+        pubksize = 256
         got = self.request.recv(pubksize)
         if len(got) < pubksize:
             self.request.sendall(b'\x00\x0EBad PubK Size\x00')
@@ -80,13 +80,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             raise ServerException('Not enough bytes got')
         ID_to = int.from_bytes(got, byteorder='big')
 
-        got = self.request.recv(8)
-        if len(got) < 8:
+        got = self.request.recv(4)
+        if len(got) < 4:
             self.request.sendall(b'\x00\x0EBad TStmpSize\x00')
             raise ServerException('Not enough bytes got')
         ts = int.from_bytes(got, byteorder='big')
 
         if ID_to in messages:
+            self.request.sendall(b'\x01')
             sz = len(messages[ID_to])
             needsz = True
             for mes in messages[ID_to]:
@@ -99,7 +100,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     self.request.sendall(mes.ID_from.to_bytes(pubksize, byteorder='big'))
                     self.request.sendall(len(mes.message).to_bytes(4, byteorder='big'))
                     self.request.sendall(mes.message.encode('utf-8'))
-                    self.request.sendall(ts.to_bytes(4, byteorder='big'))
+                    self.request.sendall(mes.ts.to_bytes(4, byteorder='big'))
             if needsz:
                 self.request.sendall(sz.to_bytes(4, byteorder='big'))
         else:
@@ -129,10 +130,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 self.Process_USER_INIT()
 
             if req_type is self.RequestType.SEND_MSG:
-                self.Process_USER_INIT()
+                self.Process_SEND_MSG()
             
             if req_type is self.RequestType.GET_FROMTS:
-                self.Process_USER_INIT()
+                self.Process_GET_FROMTS()
             
 
         except (ServerException,
@@ -148,90 +149,3 @@ if __name__ == "__main__":
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         server.serve_forever()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    class RequestType(Enum):
-        NO_REQ = 0
-        USER_INIT = 1
-        SEND_MSG = 2
-        GET_FROMTS = 3
-        SEND_QUEST = 4
-
-    def Process_USER_INIT(self):
-        pass
-
-    def Process_SEND_MSG(self):
-        pass
-
-    def Process_GET_FROMTS(self):
-        pass
-
-    def Process_SEND_QUEST(self):
-        pass        
-
-    def handle(self):
-        try:
-            self.request.settimeout(5)
-            got = self.request.recv(4)
-            if len(got) < 4:
-                raise ServerException('Not enough bytes got')
-            req_type_int = int.from_bytes(got, byteorder='big')
-            try:
-                req_type = self.RequestType(req_type_int)
-            except ValueError:
-                self.request.sendall(b'\x00\x10Bad RequestType\x00')
-                raise ServerException('Bad RequestType')
-            if req_type is self.RequestType.NO_REQ:
-                self.request.sendall(b'\x01\x11Request Accepted\x00')
-                return
-            if req_type is self.RequestType.USER_INIT:
-                self.Process_USER_INIT()
-
-            # data = str(self.request.recv(1024), 'ascii')
-            #cur_thread = threading.current_thread()
-            #response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-            #self.request.sendall(response)
-        except (ServerException,
-                socket.timeout,
-                OSError) as e:
-            print(e)
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "176.99.11.61", 13013
-
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    with server:
-        ip, port = server.server_address
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
-        
-        a = 'str\n'
-        input(a)
-
-        server.shutdown()
-'''
