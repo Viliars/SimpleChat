10616297df4aac7b57db484c5c176dc69dc2672a
Morgan-iv <ivushkinalex@gmail.com>
2018-12-01 11:47:00 +0300

    Server v0.0.3

--- server/server.py
+++ server/server.py
@@ -13,16 +13,9 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-def client(ip, port, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
-        sock.sendall(bytes(message, 'ascii'))
-        response = str(sock.recv(1024), 'ascii')
-        print("Received: {}".format(response))
-
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 0
+    HOST, PORT = "176.99.11.61", 13013
 
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
@@ -36,47 +29,6 @@ if __name__ == "__main__":
         server_thread.start()
         print("Server loop running in thread:", server_thread.name)
         
-        client(ip, port, "Hello World 1")
-        client(ip, port, "Hello World 2")
-        client(ip, port, "Hello World 3")
-        
+        input(a)
+
         server.shutdown()
-'''
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        bo = 'big' if int.from_bytes(self.request.recv(4), byteorder='big') == 1 else 'little'
-
-        '''
-        '''
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        '''
-        # just send back the same data, but upper-cased
-        #self.request.sendall(self.data.upper())
-        
-        # just send back the same data, but upper-cased
-        #self.request.sendall(self.data.upper())
-'''
-#def 
-
-if __name__ == "__main__":
-    HOST, PORT = "176.99.11.61", 13013
-
-    # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-    server.serve_forever()
-    #with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        #server.serve_forever()
-'''
