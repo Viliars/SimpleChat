dd48207fb9014e0eccf73b6e9e442f37e2444a73
Morgan-iv <ivushkinalex@gmail.com>
2018-12-01 14:23:52 +0300

    Server v0.0.4

--- server/server.py
+++ server/server.py
@@ -1,14 +1,55 @@
 import socket
 import threading
 import socketserver
+from enum import Enum
+
+class ServerException(Exception):
+    pass
+
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    class RequestType(Enum):
+        NO_REQ = 0
+        USER_INIT = 1
+        SEND_MSG = 2
+        GET_FROMTS = 3
+        SEND_QUEST = 4
+
+    def Process_USER_INIT(self, bo):
+        pass
+
+    def Process_SEND_MSG(self, bo):
+        pass
+
+    def Process_GET_FROMTS(self, bo):
+        pass
+
+    def Process_SEND_QUEST(self, bo):
+        pass        
 
     def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        self.request.sendall(response)
+        try:
+            self.request.settimeout(5)
+            bo = 'big' if int.from_bytes(self.request.recv(4), byteorder='big') == 1 else 'little'
+            req_type_int = int.from_bytes(self.request.recv(4), byteorder=bo)
+            try:
+                req_type = self.RequestType(req_type_int)
+            except ValueError:
+                self.request.sendall('\x00\x10Bad RequestType\x00')
+                raise ServerException('Bad RequestType')
+            if req_type is self.RequestType.NO_REQ:
+                self.request.sendall('\x01\x11Request Accepted\x00')
+                return
+            if req_type is self.RequestType.USER_INIT:
+                self.Process_USER_INIT(bo)
+
+            # data = str(self.request.recv(1024), 'ascii')
+            #cur_thread = threading.current_thread()
+            #response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+            #self.request.sendall(response)
+        except (ServerException,
+                socket.timeout) as e:
+            print(e)
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
