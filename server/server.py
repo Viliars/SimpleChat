be1f4a767c196aedc765f4d9458b0b7d2e86f50f
Morgan-iv <ivushkinalex@gmail.com>
2018-12-01 11:07:09 +0300

    Server v0.0.2

--- server/server.py
+++ server/server.py
@@ -1,5 +1,47 @@
+import socket
+import threading
 import socketserver
 
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        cur_thread = threading.current_thread()
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        self.request.sendall(response)
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+def client(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        sock.sendall(bytes(message, 'ascii'))
+        response = str(sock.recv(1024), 'ascii')
+        print("Received: {}".format(response))
+
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 0
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        ip, port = server.server_address
+
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+        
+        client(ip, port, "Hello World 1")
+        client(ip, port, "Hello World 2")
+        client(ip, port, "Hello World 3")
+        
+        server.shutdown()
+'''
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
     The request handler class for our server.
@@ -11,16 +53,20 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        #bo = 'big' if int.from_bytes(self.request.recv(4), byteorder='big') == 1 else 'little'
+        bo = 'big' if int.from_bytes(self.request.recv(4), byteorder='big') == 1 else 'little'
+
+        '''
+        '''
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
+        '''
         # just send back the same data, but upper-cased
         #self.request.sendall(self.data.upper())
         
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
-
+        #self.request.sendall(self.data.upper())
+'''
 #def 
 
 if __name__ == "__main__":
@@ -33,3 +79,4 @@ if __name__ == "__main__":
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         #server.serve_forever()
+'''
